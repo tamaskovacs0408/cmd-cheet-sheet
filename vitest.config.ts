0b28b48adbe03ad862cf0239e45b1e8c44fc7244
Tamás Kovács <kovacs.tamas0408@gmail.com>
2026-03-27 22:33:35 +0100

    Project init + add setups and configs

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    css: true,
+  },
+});
