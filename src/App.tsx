dc100724a226e13b219022aceddfbde702726def
Tamas Kovacs <kovacs.tamas0408@gmail.com>
2026-09-07 20:38:45 +0200

    Update import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router";
-import { ThemeProvider } from "@/context/ThemeContext";
+import { ThemeProvider } from "@/context/ThemeProvider";
 import { router } from "@/router";
 
 export default function App() {
